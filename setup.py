254ff5f6c391725d43948893cd6bd0fa20dbeff7
torminator <torben.schick@adesso.de>
2019-06-01 18:47:21 +0200

    some reorganization

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='markovchains',
+   version='0.1',
+   description='This is a general purpose simple markov chain implementation.',
+   author='Schick, Torben',
+   packages=['markovchains'],
+   install_requires=['numpy'], 
+)
